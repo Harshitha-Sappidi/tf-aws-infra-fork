761a0b791a6048f75c4dc56e1e3fd9c8c02f2f80
HarshithaSappidi <157057782+Harshitha-Sappidi@users.noreply.github.com>
2025-02-25 15:40:02 -0500

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "public" {
   count                   = length(var.public_subnet_cidrs)
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = aws_vpc.main.bad
   cidr_block              = var.public_subnet_cidrs[count.index]
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
