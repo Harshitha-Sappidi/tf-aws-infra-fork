ff26ba485288df9bbd5b314c6e5363ed1be14f03
Harshitha Sappidi <sappidi.h@northeastern.edu>
2025-02-27 01:23:17 -0500

    ec2, variables, sg file changes

---
+++ ec2.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "web" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = aws_subnet.public[0].id
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+
+  root_block_device {
+    volume_size = var.root_volume_size
+    volume_type = var.root_volume_type
+    delete_on_termination = true
+  }
+
+  disable_api_termination = false
+  
+  tags = {
+    Name = var.instance_name
+  }
+}
+
